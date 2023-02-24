a98695544827d0057d96467cd05b67d669069be2
Venkatesh <venkateshkoppar@gmail.com>
2023-02-24 20:16:09 +0530

    Added Background Animation

--- frontend/expense-tracker/src/App.js
+++ frontend/expense-tracker/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import bg from "../src/img/bg.png";
+import Orb from "./Components/Orb/Orb";
+import { MainLayout } from "./styles/Layout";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <AppStyled className="App" bg={bg}>
+      <Orb></Orb>
+      <MainLayout></MainLayout>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+  position: relative;
+`;
